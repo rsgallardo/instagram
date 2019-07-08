071423b7e31b9be984c64d9a38bd3bcea3552f4a
Robert Gallardo <rgallardo@fb.com>
2019-07-08 15:29:57 -0700

    Initial Commit

---
+++ instagram/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  instagram
+//
+//  Created by rgallardo on 7/8/19.
+//  Copyright © 2019 rgallardo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ instagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  instagram
+//
+//  Created by rgallardo on 7/8/19.
+//  Copyright © 2019 rgallardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
